7d61ee1014e0dc8084d1bca8f51293219ddde346
michael <michael@michael-VPCEH25EN>
2014-11-13 18:34:07 +0530

    updated by mike

---
+++ src/model/EventDrivenRequirement.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Krish
+ * Req Syntax : WHEN <optional preconditions> <trigger> the <system name> shall <system response>
+ */
+public class EventDrivenRequirement extends EARSRequirement{
+    private String _reqOptionalPreCond;
+    private String _reqTrigger;
+    private String _reqSysName;
+    private String _reqSysResponse;
+
+    public String getReqOptionalPreCond() {
+        return _reqOptionalPreCond;
+    }
+
+    public String getReqTrigger() {
+        return _reqTrigger;
+    }
+
+    public String getReqSysName() {
+        return _reqSysName;
+    }
+
+    public String getReqSysResponse() {
+        return _reqSysResponse;
+    }
+
+    public void setReqOptionalPreCond(String _reqOptionalPreCond) {
+        this._reqOptionalPreCond = _reqOptionalPreCond;
+    }
+
+    public void setReqTrigger(String _reqTrigger) {
+        this._reqTrigger = _reqTrigger;
+    }
+
+    public void setReqSysName(String _reqSysName) {
+        this._reqSysName = _reqSysName;
+    }
+
+    public void setReqSysResponse(String _reqSysResponse) {
+        this._reqSysResponse = _reqSysResponse;
+    }
+}
---
+++ src/model/UbiquitousRequirement.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Krish
+ * Req Syntax : The <system name> shall <system response>
+ */
+public class UbiquitousRequirement extends EARSRequirement{
+    private String _reqSysName;
+    private String _reqSysResponse;
+
+    public void setReqSysName(String _reqSysName) {
+        this._reqSysName = _reqSysName;
+    }
+
+    public void setReqSysResponse(String _reqSysResponse) {
+        this._reqSysResponse = _reqSysResponse;
+    }
+
+    public String getReqSysName() {
+        return _reqSysName;
+    }
+
+    public String getReqSysResponse() {
+        return _reqSysResponse;
+    }
+}
